3b4e597d70ef4e450e8365b93f65d0ee30a7a13e
piotrjwegrzyn <piotrwegrzyn@protonmail.com>
2022-09-13 12:09:43 +0200

    Step procedure for generating EEPROM

--- generator.go
+++ generator.go
@@ -9,14 +9,14 @@ func main() {
 
 	var scenarioFilename = flag.String("scenario", "scenario.yaml", "Location of scenario yaml config")
 	var modulesFilename = flag.String("modules", "modules.yaml", "Location of modules yaml config")
+	var outputPath = flag.String("outputPath", ".", "Output of EEPROM files")
 
 	flag.Parse()
 
 	scenarioConfig := opers.GetScenarioConfig(*scenarioFilename)
 	modulesConfig := opers.GetModulesConfig(*modulesFilename)
 
-	data := [][]byte{opers.CreateTimelapse(modulesConfig.Modules[0], scenarioConfig.ScenarioModules[0], scenarioConfig.Duration)}
-
-	opers.EepromToFiles("./", "tmp", data)
+	opers.EepromToFiles(*outputPath, "module1", opers.CreateTimelapse(modulesConfig.Modules[0], scenarioConfig.ScenarioModules[0], scenarioConfig.Duration))
+	opers.EepromToFiles(*outputPath, "module2", opers.CreateTimelapse(modulesConfig.Modules[1], scenarioConfig.ScenarioModules[1], scenarioConfig.Duration))
 
 }
--- opers/timelapse.go
+++ opers/timelapse.go
@@ -1,19 +1,54 @@
 package opers
 
-func CreateTimelapse(module Module, scenario ScenarioModule, duration int) (timelapse []byte) {
+import (
+	"fmt"
+)
 
-	currentTemperature := scenario.Temperature[0].Endval
-	currentVoltage := scenario.Voltage[0].Endval
+func generateSteps(steps []Step, duration int) (listS []int) {
+	for i := 0; i < len(steps); i++ {
+		if i == 0 {
+			for j := 0; j < steps[i].Duration; j++ {
+				listS = append(listS, steps[i].Endval)
+			}
+		} else {
+			f := func(x int) int {
+				return (steps[i].Endval-steps[i-1].Endval)*(x+1)/steps[i].Duration + steps[i-1].Endval
+			}
+			for j := 0; j < steps[i].Duration; j++ {
+				listS = append(listS, f(j))
+			}
+		}
+	}
 
-	timelapse = append(timelapse, GeneratePageLow(module, float32(currentTemperature/10), uint16(currentVoltage))...)
-	timelapse = append(timelapse, GeneratePage00h(module)...)
-	timelapse = append(timelapse, GeneratePage01h()...)
-	timelapse = append(timelapse, GeneratePage02h(module)...)
-	timelapse = append(timelapse, GeneratePage04h(module)...)
-	timelapse = append(timelapse, GeneratePage10h()...)
-	timelapse = append(timelapse, GeneratePage11h()...)
-	timelapse = append(timelapse, GeneratePage12h(module)...)
-	timelapse = append(timelapse, GeneratePage25h(scenario.Osnr[0].Endval, currentTemperature/10)...)
+	if len(listS) != duration {
+		fmt.Printf("Error: mismatch between ScenarioDuration (%d) and steps durations (%d) near %d endval\n", duration, len(listS), listS[len(listS)-1])
+		panic(1)
+	}
+
+	return
+}
+
+func CreateTimelapse(module Module, scenario ScenarioModule, duration int) (timelapse [][]byte) {
+
+	listVcc := generateSteps(scenario.Voltage, duration)
+	listTemp := generateSteps(scenario.Temperature, duration)
+	// listTxPower := generateSteps(scenario.TxPower, duration)
+	// listRxPower := generateSteps(scenario.RxPower, duration)
+	listOsnr := generateSteps(scenario.Osnr, duration)
+
+	for i := 0; i < duration; i++ {
+		timelapseStep := make([]byte, 0)
+		timelapseStep = append(timelapseStep, GeneratePageLow(module, float32(listTemp[i]/10), uint16(listVcc[i]))...)
+		timelapseStep = append(timelapseStep, GeneratePage00h(module)...)
+		timelapseStep = append(timelapseStep, GeneratePage01h()...)
+		timelapseStep = append(timelapseStep, GeneratePage02h(module)...)
+		timelapseStep = append(timelapseStep, GeneratePage04h(module)...)
+		timelapseStep = append(timelapseStep, GeneratePage10h()...)
+		timelapseStep = append(timelapseStep, GeneratePage11h()...)
+		timelapseStep = append(timelapseStep, GeneratePage12h(module)...)
+		timelapseStep = append(timelapseStep, GeneratePage25h(listOsnr[i], listTemp[i]/10)...)
+		timelapse = append(timelapse, timelapseStep)
+	}
 
 	return
 }
